7c4e1a82f96c7a074ab697731aa22a2f3ac5f741
intisolar <insolgar@gmail.com>
2021-11-03 21:19:06 -0300

    Arreglé la Query que tenía mal el atributo id

--- src/main/java/com/web/fixture/repositorios/PartidoEliminatorioRepositorio.java
+++ src/main/java/com/web/fixture/repositorios/PartidoEliminatorioRepositorio.java
@@ -6,11 +6,14 @@
 package com.web.fixture.repositorios;
 
 import com.web.fixture.entidades.PartidoEliminatorio;
+import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PartidoEliminatorioRepositorio extends JpaRepository<PartidoEliminatorio, Integer>{
-//        @Query("SELECT * FROM PartidoEliminatorio c WHERE c.fase = :fase ORDER BY c.id_partido asc")
-//    public ArrayList<PartidoEliminatorio> buscarPartidosxFase(@Param("fase") String grupo);
+        @Query("SELECT c FROM PartidoEliminatorio c WHERE c.fase = :fase ORDER BY c.idPartido ASC")
+    public ArrayList<PartidoEliminatorio> buscarPartidosxFase(@Param("fase") String fase);
 }
